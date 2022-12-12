794f5832bb702e93b30a5f4ace82bf36da8dff04
YusifHuseynzade <110456474+YusifHuseynzade@users.noreply.github.com>
2022-12-13 01:43:28 +0400

    Pustok
    Add

---
+++ PustokDb2022/PustokDb2022/Areas/Manage/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PustokDb2022.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ PustokDb2022/PustokDb2022/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PustokDb2022.DAL;
+using PustokDb2022.Models;
+using PustokDb2022.ViewModels;
+
+namespace PustokDb2022.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly PustokDbContext _pustokcontext;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(PustokDbContext pustokContext, UserManager<AppUser> userManager)
+        {
+            _pustokcontext = pustokContext;
+            _userManager= userManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync(MemberRegisterViewModel memberVM)
+        {
+            if(!ModelState.IsValid)
+                return View();
+            //AppUser user = _pustokdbcontext.AppUsers.FirstOrDefault(x=>x.NormalizedUserName == memberVM.UserName.ToUpper());
+
+            if (await _userManager.FindByNameAsync(memberVM.UserName) != null)
+            {
+                ModelState.AddModelError("Username", "This Username already exist!");
+                return View();
+            }
+            else if (await _userManager.FindByEmailAsync(memberVM.Email) != null)
+            {
+                ModelState.AddModelError("Email", "This Email already exist!");
+                return View();
+            }
+
+            AppUser user = new AppUser
+            {
+                UserName = memberVM.UserName,
+                Email = memberVM.Email,
+                FullName= memberVM.FullName,
+            };
+
+            var result = await _userManager.CreateAsync(user, memberVM.Password);
+
+           if(!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+            }
+
+            return RedirectToAction("login");
+        }
+    }
+}
